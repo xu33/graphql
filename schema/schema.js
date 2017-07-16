a8fc151c43df5c8f82d13de55c750c4a8005736f
hehe <emulephpwyj@gmail.com>
2017-07-16 14:19:21 +0800

    update

---
+++ schema/schema.js
@@ -0,0 +1,82 @@
+const graphql = require('graphql');
+const axios = require('axios');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
+
+const users = [
+  {
+    id: '23', firstName: 'Bill', age: 20
+  },
+  {
+    id: '47',
+    firstName: 'Samantha',
+    age: 21
+  }
+];
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: {
+      type: GraphQLString
+    },
+    name: {
+      type: GraphQLString
+    },
+    description: {
+      type: GraphQLString
+    }
+  }
+})
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: {
+      type: GraphQLString
+    },
+    firstName: {
+      type: GraphQLString
+    },
+    age: {
+      type: GraphQLInt
+    },
+    // 关联
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        // console.log(parentValue, args);
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`).
+          then(res => res.data);
+      }
+    }
+  }
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: {
+          type: GraphQLString
+        }
+      },
+      resolve(parentValue, args) {
+        // return users.find(user => user.id == args.id)
+
+        return axios.get(`http://localhost:3000/users/${args.id}`).then(rsp => rsp.data);
+        // .then(response => console.log(response)) // {data:{firstName: 'bill'}}
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
